a2a0d2ca1175888ad4d6582b7bda655ea1168f65
poouo <91967298+poouo@users.noreply.github.com>
2026-06-29 16:29:01 +0800

    release: v2.3.195

--- shared/versions.ts
+++ shared/versions.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = "2.3.194";
-export const ANDROID_APP_VERSION = "2.3.65";
-export const ANDROID_APK_RELEASE_VERSION = "2.3.194";
-export const AGENT_VERSION = "2.2.122";
+export const APP_VERSION = "2.3.195";
+export const ANDROID_APP_VERSION = "2.3.66";
+export const ANDROID_APK_RELEASE_VERSION = "2.3.195";
+export const AGENT_VERSION = "2.2.123";
